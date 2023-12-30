6034225e0776a66fcd39d81c418edf384f7b12ae
Eileen Ilsemann <eileenilsemanni@gmail.com>
2023-12-30 01:54:15 +0100

    add solution

--- js/index.js
+++ js/index.js
@@ -5,7 +5,21 @@ Rewrite the following functions as arrow functions.
 Use implicit returns when possible
 */
 
-function getCurrentHour() {
+// function getCurrentHour() {
+//   const now = new Date();
+//   const currentHour = now.getHours();
+//   if (currentHour === 0) {
+//     return "12am";
+//   } else if (currentHour === 12) {
+//     return "12pm";
+//   } else if (currentHour <= 11) {
+//     return currentHour + "am";
+//   } else {
+//     return currentHour - 12 + "pm";
+//   }
+// }
+
+const getCurrentHour = () => {
   const now = new Date();
   const currentHour = now.getHours();
   if (currentHour === 0) {
@@ -17,21 +31,34 @@ function getCurrentHour() {
   } else {
     return currentHour - 12 + "pm";
   }
-}
+};
 
-function getVectorLength(x, y, z) {
-  return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
-}
+// function getVectorLength(x, y, z) {
+//   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
+// }
+const getVectorLength = (x, y, z) => (x ** 2 + y ** 2 + z ** 2) ** 0.5;
 
-function cleanInput(string) {
-  return string.toLowerCase().trim();
-}
+// function cleanInput(string) {
+//   return string.toLowerCase().trim();
+// }
+const cleanInput = (string) => string.toLowerCase().trim();
 
 /*
 Rewrite the following arrow functions as classic functions.
 */
 
-const isOddAndSmall = (number) => {
+// const isOddAndSmall = (number) => {
+//   if (number > 10) {
+//     return false;
+//   }
+
+//   if (number % 2 === 0) {
+//     return false;
+//   }
+
+//   return true;
+// };
+function isOddAndSmall(number) {
   if (number > 10) {
     return false;
   }
@@ -41,8 +68,23 @@ const isOddAndSmall = (number) => {
   }
 
   return true;
-};
+}
 
-const add3 = (a, b, c) => a + b + c;
+// const add3 = (a, b, c) => a + b + c;
+function add3(a, b, c) {
+  return a + b + c;
+}
 
-const repeat10 = (string) => string.repeat(10);
+// const repeat10 = (string) => string.repeat(10);
+function repeat10(string) {
+  return string.repeat(10);
+}
+
+//um herauszufinden, ob ich das alles richtig gemacht habe calle ich jetzt nochmal jede function...
+
+console.log(getCurrentHour());
+console.log(getVectorLength(1, 2, 3));
+console.log(cleanInput("hAlLLoWaaS"));
+console.log(isOddAndSmall(801));
+console.log(add3(1, 2, 3));
+console.log(repeat10("ha")); // joa.. ich haette jetzt gesagt das passt so...
